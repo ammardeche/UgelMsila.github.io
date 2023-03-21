da37a642465ecb8b476f9ea45ca7b4238a29d8cd
ammardeche <102583850+ammardeche@users.noreply.github.com>
2023-03-21 22:25:31 +0100

    Upload

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const apiUrl = 'http://ammar-faculty.babylontech.net';
-const xhr = new XMLHttpRequest();
+async function postData(url = "http://ammar-faculty.babylontech.net/SendMail", data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      'Access-Control-Allow-Origin': '*'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
 
-xhr.open('GET', apiUrl);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === XMLHttpRequest.DONE) {
-    if (xhr.status === 200) {
-      const data = JSON.parse(xhr.responseText);
-      console.log(data);
-    } else {
-      console.error('Error retrieving data from API.');
-    }
-  }
-};
-xhr.send();
+postData("http://ammar-faculty.babylontech.net/SendMail", { answer: 42 }).then((data) => {
+  console.log(data); // JSON data parsed by data.json() call
+});
